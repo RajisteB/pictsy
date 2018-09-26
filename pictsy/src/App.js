66f44a9d86e49938340a4c2e9bdeae49fe3a3149
Raj Bennin <rajisteb@gmail.com>
2018-09-26 16:41:52 -0400

    added image filter by dataset

--- pictsy/src/App.js
+++ pictsy/src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import ImageFilter from './components/filter/imageFilter.js';
 import NavBar from './components/navigation/navbar.js';
+import { IMGUR_CLIENT_KEY } from './config.js';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [],
+    }
+  }
+
+  getImgurPhotos = () => {
+    let imageGridArr;
+    axios({
+      method: 'get',
+      baseURL: 'https://api.imgur.com/3/gallery/t/photography/top/month?mature=true&album_previews=true',
+      headers: { 'Authorization': 'Client-ID ' + IMGUR_CLIENT_KEY }
+    })
+    .then(res => {
+      imageGridArr = res.data.data.items.slice();
+      this.setState({
+        images: imageGridArr
+      })
+      console.log(res.data.data);
+    })
+    .catch(err => console.log(err));
+  }
+
+  componentDidMount() {
+    this.getImgurPhotos();
+  }
+
   render() {
+    let { images } = this.state;
     return (
       <div className="App">
         <NavBar/>
+        <div className="photo-grid-container">
+          <ImageFilter images={images}/>
+        </div>
       </div>
     );
   }
---
+++ pictsy/src/components/filter/imageFilter.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+const Main = (props) => <div className="title">{props.children}</div>
+
+class ImageFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.activeRef = [];
+  }
+
+  handleFilterClick = (id) => {
+    this.activeRef.map((a,i) => {
+      a.style.opacity = 1;
+      a.style.background = 'black';
+    })
+    this.activeRef[id].style.opacity = 0.4;
+    this.activeRef[id].style.background = 'green';
+  }
+
+  componentDidMount() {
+    this.handleFilterClick(0);
+  }
+  
+  render() {
+    const FilteredList = {
+      Date: 'datetime',
+      Comments: 'comment_count',
+      Upvotes: 'ups',
+      Downvotes: 'downs',
+      Favorites: 'favorite_count'
+    }
+
+    return (
+      <Main>
+        <h1>Popular Photos</h1>
+        <h2>Images with the most views in the past 30 days.</h2>
+        <ul className="filter-list">
+          <li style={{ color: '#888' }}>Sort By:</li>
+          {
+            Object.keys(FilteredList).map((f, idx) => {
+              return (
+                <li 
+                  key={idx}
+                  className="filter-item" 
+                  onClick={() => this.handleFilterClick(idx)}
+                  ref={(ref) => this.activeRef[idx] = ref}
+                >{f}</li>
+              )
+            })
+          }
+        </ul>
+      </Main>
+    )
+  }
+}
+
+export default ImageFilter
